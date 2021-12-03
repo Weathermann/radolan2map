ddac299d81dd156eff5321077aa7258d108d6f07
Weathermann <radolan2map@e.mail.de>
2021-12-03 22:48:30 +0100

    integrate regnie2raster functionality

--- classes/ActionTabRegnie.py
+++ classes/ActionTabRegnie.py
@@ -1,12 +1,13 @@
-'''
+"""
 ActionTabRegnie
 
 Separate __actions__ of REGNIE operations from the other components
 
 Created on 01.12.2020
 @author: Weatherman
-'''
+"""
 
+import os
 from pathlib import Path
 
 from qgis.PyQt.QtWidgets import QFileDialog
@@ -15,6 +16,7 @@ from qgis.PyQt.QtWidgets import QFileDialog
 from ActionTabBase  import ActionTabBase         # base class
 from Regnie2CSV     import Regnie2CSV
 from LayerLoader    import LayerLoader
+import regnie2raster as r2r
 
 
 
@@ -47,7 +49,7 @@ class ActionTabRegnie(ActionTabBase):
         start_dir = Path(text).parent if text  else Path.home()
         
         # after title string: start path, file_filter
-        regnie_file, _ = QFileDialog.getOpenFileName(self.dock, "Please select a REGNIE raster file",
+        regnie_file, _ = QFileDialog.getOpenFileName(self.dock, "Please select a REGNIE file",
                                                        str(start_dir), None,
         # these additional parameters are used, because QFileDialog otherwise doesn't start with the given path:
                                                        None, QFileDialog.DontUseNativeDialog)
@@ -68,7 +70,7 @@ class ActionTabRegnie(ActionTabBase):
         
         # field was leaved empty
         if not regnie_file:
-            super()._show_critical_message_box("REGNIE raster input file wasn't specified!")
+            super()._show_critical_message_box("REGNIE input file wasn't specified!")
             dock.btn_load_regnie.setEnabled(False)
             return
         
@@ -76,7 +78,7 @@ class ActionTabRegnie(ActionTabBase):
         regnie_file = Path(regnie_file)
         
         if not regnie_file.exists():
-            super()._show_critical_message_box( "File\n'{}'\nnot found!".format(regnie_file) )
+            super()._show_critical_message_box(f"File\n'{regnie_file}'\nnot found!")
             dock.btn_load_regnie.setEnabled(False)
             return
         
@@ -87,7 +89,7 @@ class ActionTabRegnie(ActionTabBase):
         try:  # include everything that can raise exceptions
             try:
                 self._model.create_storage_folder_structure()
-                self.out("create data dir for converted REGNIE: '{}'".format(data_dir))
+                self.out(f"create data dir for converted REGNIE: '{data_dir}'")
             except FileExistsError:
                 pass
             
@@ -100,9 +102,38 @@ class ActionTabRegnie(ActionTabBase):
             55.058333,8.383333,1,7
             55.058333,8.400000,2,7
             '''
+
+            show_csv = True
+            # but CSV is only displayed in case of errors (fallback) because raster format is better
+            # but True is better here because of exception handling (set False at one place)
+            
+            # convert to raster
+            # TODO: assumes the file has already been decompressed from gz!
+
+            self.out("Starting REGNIE to raster conversion...")
+            regnie_raster_file = data_dir / f"{regnie_file.name}.tif"
+
+            try:
+                r2r.regnie2raster(regnie_file, str(regnie_raster_file))
+            # Exceptions,
+            # but nevertheless load the CSV file:
+            except ModuleNotFoundError as e:
+                """ Under Linux a "ModuleNotFoundError: No module named '_gdal'"
+                error can occur. """
+                msg = f"Exception: {e}:\nPossibly a GDAL installation error on Linux(?)"
+                regnie_raster_file = "/run/media/loki/ungesichert/Testdaten/REGNIE/raster/RASA1908.tif"    # for testing
+                super()._show_critical_message_box(msg, 'REGNIE raster conversion error')
+                self.out(f"{msg}\nload REGNIE test raster file: {regnie_raster_file}", False)
+            except Exception as e:
+                msg = f"Exception: {e}"
+                self.out(f"{msg}")
+                super()._show_critical_message_box(msg, 'REGNIE raster conversion error')
+            else:
+                self.out(f"Successfully created REGNIE raster file {regnie_raster_file}!")
+                show_csv = False
             
         except Exception as e:
-            msg = "{}, wrong format!".format(str(e))
+            msg = f"{e}, wrong format!"
             super()._show_critical_message_box(msg, 'Layer loading error')  # disable here, because it was only set a folder
             dock.btn_load_regnie.setEnabled(False)
             # Reset - don't save the wrong data for the next run!:
@@ -111,27 +142,30 @@ class ActionTabRegnie(ActionTabBase):
             return
         
         
-        
         """
-        Add CSV data
+        Add the layers ...
         """
-        
+
         # Determine prepared QML-File delivered with the plugin:
-        
-        self.out("char period indicator = '{}'".format(char_period_indicator))
-        
+        self.out(f"char period indicator = '{char_period_indicator}'")
+
         d_regnie_qml = {
-            'd': 'regnie_daily.qml',
-            'm': 'regnie_monthly.qml',
-            'y': 'regnie_yearly.qml',
-            }
-        
-        qml_file = self._model.symbology_path / d_regnie_qml[char_period_indicator]
-        self.out("qml: '{}'".format(qml_file))
-        
-        
-        ll = LayerLoader(self._iface)    # 'iface' is from 'radolan2map'
-        ll.load_vector(regnie_csv_file, qml_file)    # incompatible with Path()!
+            'd': ("regnie_raster_daily.qml", 'regnie_daily.qml'),
+            'm': ("regnie_raster_monthly.qml", 'regnie_monthly.qml'),
+            'y': ("regnie_raster_yearly.qml", 'regnie_yearly.qml'),
+        }
+
+        ll = LayerLoader(self._iface)  # 'iface' is from 'radolan2map'
+
+        # load the regnie raster file
+        qml_file = self._model.symbology_path / d_regnie_qml[char_period_indicator][0]
+        ll.load_raster(regnie_raster_file, qml_file)
+
+        if show_csv:
+            # load the CSV file
+            qml_file = self._model.symbology_path / d_regnie_qml[char_period_indicator][1]
+            ll.load_vector(regnie_csv_file, qml_file)    # incompatible with Path()!
+
         
         filename, dim, _max, _min, mean, total, valid, non_valid = t_statictics
         filename = filename.replace('.gz', '')    # maybe 'RASA1908.gz'
@@ -140,7 +174,7 @@ class ActionTabRegnie(ActionTabBase):
         dock.text_shape.setText(dim)
         dock.text_max.setText(str(_max))
         dock.text_min.setText(str(_min))
-        dock.text_mean.setText("{:.1f}".format(mean))
+        dock.text_mean.setText(f"{mean:.1f}")
         dock.text_total_pixels.setText(str(total))
         dock.text_valid_pixels.setText(str(valid))
         dock.text_nonvalid_pixels.setText(str(non_valid))
@@ -158,7 +192,7 @@ class ActionTabRegnie(ActionTabBase):
         can raise Exception
         '''
         
-        self.out("__convert_regnie_raster2csv('{}')".format(regnie_raster_file))
+        self.out(f"__convert_regnie_raster2csv('{regnie_raster_file}')")
         
         
         regnie_converter = Regnie2CSV(regnie_raster_file)
