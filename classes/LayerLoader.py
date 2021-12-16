66446df9f55d7c732c080353047e0340ca2a463f
Tobias Rosskopf <tobirosskopf@gmail.com>
2021-12-16 13:16:24 +0100

    Changed flake8 configuration; Formated with black; Stylecheck with flake8

--- classes/LayerLoader.py
+++ classes/LayerLoader.py
@@ -1,11 +1,11 @@
-'''
+"""
 LayerLoader
 
 Loads creates and load a QgsRaster- or QgsVectorLayer
 
 Created on 07.12.2020
 @authors: Weatherman, Tobias Rosskopf
-'''
+"""
 
 import re
 import sys
@@ -30,7 +30,6 @@ class LayerLoader:
     classdocs
     """
 
-
     def __init__(self, iface):
         """
         Constructor
@@ -41,20 +40,17 @@ class LayerLoader:
         self._iface = iface
 
         # set or determined later:
-        self._no_zeros = False    # set zeros invisible
+        self._no_zeros = False  # set zeros invisible
         self._layer_name = None
-
 
     def __str__(self):
         return self.__class__.__name__
-
 
     def out(self, s, ok=True):
         if ok:
             print("{}: {}".format(self, s))
         else:
             print("{}: {}".format(self, s), file=sys.stderr)
-
 
     def _show_message(self, qgis_state, layer_name, duration):
 
@@ -66,8 +62,9 @@ class LayerLoader:
             msg = "Layer '{}' failed to load!".format(layer_name)
             self.out(msg, False)
 
-        self._iface.messageBar().pushMessage(title, msg, level=qgis_state, duration=duration)
-
+        self._iface.messageBar().pushMessage(
+            title, msg, level=qgis_state, duration=duration
+        )
 
     def load_raster(self, tif_file, qml_file=None):
 
@@ -85,7 +82,6 @@ class LayerLoader:
         """
         The new raster layer is ok and we will continue to work with it in the following
         """
-
 
         """
         --- Clean: If necessary remove existing layer ---
@@ -135,22 +131,22 @@ class LayerLoader:
 
         self._insert_layer(raster_layer, qml_file, 3)
 
-
     def load_vector(self, csv_file, qml_file=None):
         # uri = "file:///{}?encoding=UTF-8&delimiter=,&xField=LON&yField=LAT&crs=EPSG:4326".format(regnie_csv_file)
         # -> was working, simpler variant:
         # uri = "{}{}?delimiter=,&xField=LON&yField=LAT".format('file:///', regnie_csv_file)
         # but not on Windows - needs crs specified:
-        uri = "{}{}?delimiter=,&xField=LON&yField=LAT&crs=EPSG:4326".format('file:///', csv_file)
+        uri = "{}{}?delimiter=,&xField=LON&yField=LAT&crs=EPSG:4326".format(
+            "file:///", csv_file
+        )
         self.out("uri: {}".format(uri))
 
         # Make a vector layer:
-        csv_layer = QgsVectorLayer(uri, csv_file.name, 'delimitedtext')
+        csv_layer = QgsVectorLayer(uri, csv_file.name, "delimitedtext")
 
         if not csv_layer.isValid():
             self._show_message(Qgis.Critical, csv_file.name)
             return
-
 
         layer_name = Path(csv_file).stem
         self._layer_name = layer_name
@@ -158,14 +154,14 @@ class LayerLoader:
         self._remove_layer_with_same_name(layer_name)
         self._insert_layer(csv_layer, qml_file, 5)
 
-
     def _remove_layer_with_same_name(self, layer_name):
         layers = QgsProject.instance().mapLayersByName(layer_name)
 
         for layer in layers:
-            self.out('Layer with existing name "{}" found - removing.'.format(layer.name()))
+            self.out(
+                'Layer with existing name "{}" found - removing.'.format(layer.name())
+            )
             QgsProject.instance().removeMapLayer(layer.id())
-
 
     def _insert_layer(self, layer, qml_file, duration):
 
@@ -180,7 +176,7 @@ class LayerLoader:
         # Set opacity - also for black white (without QML):
         # Sets the opacity for the layer, where opacity is a value
         # between 0 (totally transparent) and 1.0 (fully opaque).
-        opa = 0.6    # 0.6 = 40% transparency
+        opa = 0.6  # 0.6 = 40% transparency
 
         if isinstance(layer, QgsRasterLayer):
             layer.renderer().setOpacity(opa)
@@ -197,12 +193,13 @@ class LayerLoader:
         if Qgis.QGIS_VERSION_INT >= 31400:
             self._set_time_range(layer)
 
-
         # Insert layer at a certain position
 
         # Add the layer to the QGIS Map Layer Registry (the second argument must be set to False
         # to specify a custom position:
-        QgsProject.instance().addMapLayer(layer, False)  # first add the layer without showing it
+        QgsProject.instance().addMapLayer(
+            layer, False
+        )  # first add the layer without showing it
 
         # obtain the layer tree of the top-level group in the project
         layerTree = self._iface.layerTreeCanvasBridge().rootGroup()
@@ -216,20 +213,18 @@ class LayerLoader:
 
         self._show_message(Qgis.Success, layer.name(), duration)
 
-
     def _set_zeroes_invisible(self, layer):
         self.out("setting zeroes to 100% transparency")
-        '''
+        """
         # Set 0 values to NODATA (= transparent):
         provider = active_raster_layer.dataProvider()
         provider.setNoDataValue(1, 0)    # first one is referred to band number
         # -> is working
-        '''
+        """
         # better, conserves 0 value:
         tr = QgsRasterTransparency()
         tr.initializeTransparentPixelList(0)
         layer.renderer().setRasterTransparency(tr)
-
 
     def _set_qml(self, layer, qml_file):
         """
@@ -239,7 +234,7 @@ class LayerLoader:
         self.out("using QML file '{}'".format(qml_file))
 
         # if layer.geometryType() == QGis.Point:
-        layer.loadNamedStyle(str(qml_file))    # str() if Path
+        layer.loadNamedStyle(str(qml_file))  # str() if Path
 
         # if hasattr(raster_layer, "setCacheImage"):    # OK, 09.12.2020
         try:
@@ -247,11 +242,10 @@ class LayerLoader:
         except AttributeError:
             pass
 
-        layer.triggerRepaint()    # muss aufgerufen werden, Layer bleibt sonst schwarzweiß
+        layer.triggerRepaint()  # muss aufgerufen werden, Layer bleibt sonst schwarzweiß
         # Das ist für die QML-Farbskala im Layerfenster:
         # self._iface.legendInterface().refreshLayerSymbology(active_raster_layer)    # QGIS 2
         self._iface.layerTreeView().refreshLayerSymbology(layer.id())
-
 
     def _set_time_range(self, layer: QgsRasterLayer) -> None:
         """
@@ -263,13 +257,16 @@ class LayerLoader:
         time_delta = self._extract_time_delta_from_layer_name(layer.name())
         timestamp_end = self._extract_timestamp_from_layername(layer.name())
         timestamp_start = timestamp_end - time_delta
-        self.out(f"Time range from {timestamp_start:%d.%m.%Y %H:%M} to {timestamp_end:%d.%m.%Y %H:%M}.")
+        self.out(
+            f"Time range from {timestamp_start:%d.%m.%Y %H:%M} to {timestamp_end:%d.%m.%Y %H:%M}."
+        )
 
         temp_props = layer.temporalProperties()
         temp_props.setMode(QgsRasterLayerTemporalProperties.ModeFixedTemporalRange)
-        temp_props.setFixedTemporalRange(QgsDateTimeRange(timestamp_start, timestamp_end))
+        temp_props.setFixedTemporalRange(
+            QgsDateTimeRange(timestamp_start, timestamp_end)
+        )
         temp_props.setIsActive(True)
-
 
     def _extract_time_delta_from_layer_name(self, layername: str) -> timedelta:
         """
@@ -288,8 +285,11 @@ class LayerLoader:
         }
         product_name = layername.split("_")[0]
 
-        return timedelta(minutes=PRODUCT_DICT[product_name]) - timedelta(minute=1)  # -1 minute to avoid overlapping of layers TODO: not really effective
+        # time delta minus 1 minute to avoid overlapping of layers
+        # TODO: still overlapps with next layer, but why?
+        time_delta = PRODUCT_DICT[product_name] - 1
 
+        return timedelta(minutes=time_delta)
 
     def _extract_timestamp_from_layername(self, layername: str) -> datetime:
         """
@@ -305,7 +305,6 @@ class LayerLoader:
         groups = map(int, groups[0])
 
         return datetime(*groups)
-
 
     def _build_pyramids(self, layer: QgsRasterLayer) -> QgsRasterLayer:
         """
@@ -326,12 +325,11 @@ class LayerLoader:
             "FORMAT": 0,
             "OUTPUT": layer,
         }
-        result = processing.run('gdal:overviews', parameters)
+        result = processing.run("gdal:overviews", parameters)
 
         return QgsRasterLayer(result["OUTPUT"], layer.name())
 
     # ..................................................
-
 
     @property
     def no_zeros(self):
