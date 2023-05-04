800a32504485967cf1353dc545d02bbf94b2819a
Weathermann <60328977+Weathermann@users.noreply.github.com>
2023-05-04 16:40:24 +0200

    Update test_env.py

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,8 +1,17 @@
 # test_env.py
 
+import os
 import subprocess
+import platform
+import psutil
 
 
+def test_print_platform():
+    print(platform.platform())
+    print("Cores:", os.cpu_count())
+    mem = psutil.virtual_memory().total  # total physical memory in Bytes
+    print("Mem:", mem)
+    
 def test_system():
     cmd = "cat /etc/os-release"
     ret = subprocess.call(cmd, shell=True)
